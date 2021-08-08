93a30f07ee34bb17a9f32ff0159412af1c5fd010
Mean-Onkanya <onkanyalearning@gmail.com>
2021-08-08 22:12:44 +0700

    [👔] created route, model and body's validate for register user.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express()
+const users = require('./routes/api/users')
+const auth = require('./routes/api/auth')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+const app = express();
 
-app.get('/', (req, res) => res.send('API Running'))
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', users)
+app.use('/api/auth', auth)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
